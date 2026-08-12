7f7d95fa227045c59d0c5535481973bcc81c42b5
jeci2 <jeci2@kent.ac.uk>
2026-08-12 01:01:47 +0100

    Background service worker

--- content.js
+++ content.js
@@ -1,12 +1,3 @@
-let MichaelImages = [
-    "https://m.media-amazon.com/images/M/MV5BMTM1NjExNjg1OV5BMl5BanBnXkFtZTcwMTQ0NzIwMw@@._V1_FMjpg_UX1000_.jpg",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEDGQRt_yYFdw9AhhJj8qkg_-n9CTJ_a0pgUew0S_upw&s=10",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbvYPSBpwvdC9IP249HpnUbgkmoJLVH0waR8I7ofkQdQ&s=10",
-    
-];
-
-const img = document.getElementsByTagName("img");
-for (image of img) {
-    const index = Math.floor(Math.random() * MichaelImages.length);
-    image.src = MichaelImages[index];
-}
+chrome.webNavigation.onBeforeNavigate.addListener((details)=> {
+    console.log("Navigating to:", details.url);
+});
